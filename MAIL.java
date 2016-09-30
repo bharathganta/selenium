33cf48b21b196c60869a427b35dc066c3d6a2172
bharathganta <bharathc012@gmail.com>
2016-09-30 10:56:57 +0530

    Update MAIL.java

--- MAIL.java
+++ MAIL.java
@@ -20,7 +20,6 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
-
 public class MAIL {
 	
 			public static void main(String[] args) {
